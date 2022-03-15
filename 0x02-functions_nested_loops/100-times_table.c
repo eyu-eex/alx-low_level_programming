9f044074f9316df521ddae43f3e282c4ae90772b
eyu-eex <eyuelseyoum92@gmail.com>
2022-03-15 22:25:17 +0300

    bug fixes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,31 +9,42 @@
 
 void print_times_table(int n)
 {
-    int i, j, product;
+	int i, j, k;
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            if (j == 0)
-            {
-                _putchar(48);
-                continue;
-            }
-            product = i * j;
-            _putchar(',');
-            _putchar(' ');
-            if (product >= 10)
-            {
-                _putchar(product / 10 + 48);
-                _putchar(product % 10 + 48);
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar(product + 48);
-            }
-        }
-        _putchar('\n');
-    }
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
